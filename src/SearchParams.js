9427e872a52f6cb0f9db050a3c478cb252324d13
Joyland <joyland@gracepoints-iMac.local>
2021-10-19 10:21:30 -0700

    Component Composition

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useBreedList from "./useBreedList";
-import Pet from "./Pet";
+import Results from "./Results";
+
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -73,14 +74,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets = {pets} />
     </div>
   );
 };
