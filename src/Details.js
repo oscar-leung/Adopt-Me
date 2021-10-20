59d28b0c2a1584b11a798f63fc1e0a2183596c96
Oscar Leung <oscarleung1@gmail.com>
2021-10-20 10:13:05 -0700

    Fixed descriptions

--- src/Details.js
+++ src/Details.js
@@ -3,11 +3,8 @@ import { render } from "react-dom";
 import { withRouter } from "react-router";
 
 class Details extends Component{
-  constructor(){
-    super();
-    this.state = {loading : true};
-  }
-  
+  state = {loading : true}
+
   async componentDidMount(){
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
@@ -20,14 +17,14 @@ class Details extends Component{
     if(this.state.loading){
       return <h2>loading ...</h2>
     }
-    const {animal, breed, city, state, desciption, name } = this.state
+    const {animal, breed, city, state, description, name } = this.state
     return (
       <div className = "details">
         <div>
         <h1> {name}</h1>
         <h2> {animal} - {breed} - {city} - {state} </h2>
         <button> Adopt {name} </button>
-        <p>{desciption}</p>
+        <p>{description}</p>
         </div>
       </div>
     )
