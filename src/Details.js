4301b34aa155bfaadb1e3870e731e0c637f03ee7
Oscar Leung <oscarleung1@gmail.com>
2021-10-20 14:10:09 -0700

    On Context

--- src/Details.js
+++ src/Details.js
@@ -1,35 +1,54 @@
 import { Component } from "react";
-import { render } from "react-dom";
-import { withRouter } from "react-router";
+import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
-class Details extends Component{
-  state = {loading : true}
+class Details extends Component {
+  state = { loading: true };
 
-  async componentDidMount(){
+  async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await res.json();
-    this.setState(Object.assign({loading : false}, json.pets[0]));
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
-  render(){
-    if(this.state.loading){
-      return <h2>loading ...</h2>
+  render() {
+    if (this.state.loading) {
+      return <h2>loading … </h2>;
     }
-    const {animal, breed, city, state, description, name } = this.state
+
+    const {
+      animal,
+      breed,
+      city,
+      state,
+      description,
+      name,
+      images,
+    } = this.state;
+
     return (
-      <div className = "details">
+      <div className="details">
+        <Carousel images={images} />
         <div>
-        <h1> {name}</h1>
-        <h2> {animal} - {breed} - {city} - {state} </h2>
-        <button> Adopt {name} </button>
-        <p>{description}</p>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
 
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter {...props} />
+    </ErrorBoundary>
+  );
+}
